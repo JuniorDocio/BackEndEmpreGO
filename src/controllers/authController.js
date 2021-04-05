abc733baaf43828e3e41802ac74588c0d9f2119e
Samuel <samueldossantosti@gmail.com>
2021-04-04 23:12:15 -0300

    Foi feito a função de cadastro e de login do Usuario.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,16 +1,37 @@
 const usuario = require('./../models/usuario');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
     Cadastrar: async (req,res) => {
-        const {nome,senha} = req.body;
-        const user1 = await usuario.create(req.body);
+        const userLogin = await usuario.findOne({ where: {email: req.body.email}});
+
+        if(userLogin != null){
+            res.status(404).json({mensager: "Email já está em uso."})
+            return;
+        }
+        
+        const user = await usuario.create(req.body);
+
+        if(user==null){
+            res.status(404).json({mensager: "Não foi possivel cadastrar."})
+        }else{
+            const id = user.id;
+            const token = jwt.sign({id}, "123", {expiresIn: 3000})
+            res.status(200).json({auth:true, token: token})
+        }
     },
 
-    Login: (req,res) => {
-        res.status(404).json({
-            nome: "Junior",
-            idade: 20,
-            senha: 123455
-        })
+    Login: async (req,res) => {
+        const {email,senha} = req.body;
+        const user = await usuario.findOne({ where: {email: email, senha: senha}});
+
+        if (user === null) {
+            res.status(404).json({auth:false, token: false})
+        } else {
+            const id = user.id;
+            const token = jwt.sign({id}, "123", {expiresIn: 3000})
+            res.status(200).json({auth:true, token: token})
+        }
+        
     } 
 }
